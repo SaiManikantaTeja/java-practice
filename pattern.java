0a0d7fa2c968c883e212b3e60137af2f754b774c
SaiManikantaTeja <saimani.bongurala@gmail.com>
2023-03-03 15:52:23 +0530

    added pattern programs

---
+++ pattern.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class pattern{
+	public static void main(String[] args)
+	{
+		for(int i=5;i<=25;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println(i+"^2");
+			}
+		}
+	}
+}
---
+++ pattern3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class patter{
+	public static void main(String[] args)
+	{
+		//Scanner sc = new Scanner(System.in);
+		//int a=sc.nextInt;
+		for(int i=5;i>=-12;i--)
+		{
+			System.out.println(5+"*"+(i-1));
+		}
+	}
+}

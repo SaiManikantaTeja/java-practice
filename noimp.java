123305ef16fa79454e5557d16f0fb9f75be2b799
SaiManikantaTeja <saimani.bongurala@gmail.com>
2023-03-10 17:29:31 +0530

    added without import program

---
+++ noimp.java
@@ -0,0 +1,11 @@
+//import java.util.*;
+class noimp{
+	static java.util.Scanner sc = new java.util.Scanner(System.in);
+	static float m1(int a){
+		System.out.println(a);
+		return sc.nextFloat();
+	}
+	public static void main(String[] args){
+		System.out.println(m1(sc.nextInt()));
+	}
+}

bec85cb4dc87ed36dbb3f87733b360e9bde3bec4
SaiManikantaTeja <saimani.bongurala@gmail.com>
2023-03-29 16:03:46 +0530

    added prime number program

---
+++ programming/prime.java
@@ -0,0 +1,15 @@
+class prime
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a%2!=0)
+		{
+			System.out.print("Prime number");
+		}else{
+			System.out.print("not Prime number");
+		}
+
+	}
+}
